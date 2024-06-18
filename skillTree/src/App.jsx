6b6fbba6679487de97aa8552e59c7ceadf1c1060
Cecilia <109989926+Eon4@users.noreply.github.com>
2024-06-18 19:35:32 +0200

    Project start
    - Cleaning
    - Adding component

---
+++ skillTree/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello there</h1>
+    <h2>Lets build a skillstree</h2>
+    </>
+  )
+}
+
+export default App
